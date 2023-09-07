3019604c7359935adc22874420a63c2a46c6f6b7
csv99 <csva.99@outlook.com>
2023-09-07 04:28:16 -0300

    modified:   src/app/login/login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { AlertController, IonModal } from '@ionic/angular';
+import { ViewChild } from '@angular/core'
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
 
   constructor(private router: Router, private activatedRouter: ActivatedRoute, public alertController: AlertController) { }
 
@@ -57,6 +59,7 @@ export class LoginPage implements OnInit {
   salir(){
     this.router.navigate(['home']);
     localStorage.removeItem("Datos");
+    this.modal.dismiss(null, 'salir');
   }
 
 }
